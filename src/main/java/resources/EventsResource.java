a576f659937c2ac9adb278f8f1d89d000db6d131
myungsun <myungsun@adcnx.com>
2013-05-04 21:08:45 -0700

    EventsResource is added

---
+++ src/main/java/resources/EventsResource.java
@@ -0,0 +1,16 @@
+package resources;
+
+import java.util.List;
+
+import javax.ws.rs.Path;
+
+import models.ShirleyEvent;
+
+@Path("event")
+public class EventsResource
+{
+	public List<ShirleyEvent> getEvents() throws Exception
+	{
+		return null;
+	}
+}
